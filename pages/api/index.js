ac306e976969bbeaf8d1c6f2583ed2312826a9ab
Ismael Strey <ismaelstrey@hotmail.com>
2022-05-29 21:05:23 -0300

    enable cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import data from '../../services/naruto.json'
-
-export default function handler(req, res) {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+function handler(req, res) {
   const message = getRandom()
   res.status(200).json(message)
 }
@@ -15,3 +33,5 @@ const getRandom = () => {
   })
   return dadosFiltrados
 }
+
+export default allowCors(handler)
